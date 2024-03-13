6369414679834b08e5ba4d77d53b96a16eecefe5
Ucopyrightable <132915653+Ucopyrightable@users.noreply.github.com>
2024-03-13 23:17:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,5 +22,5 @@
 # Modify fireware
 rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/{alist,adguardhome,smartdns}
 chmod -R 755 files
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
